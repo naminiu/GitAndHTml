2877d4a38de2cb53aa026b06f8b3ec1d943bde85
luulo123123 <luulo3527@gmail.com>
2022-09-11 10:48:51 +0700

    sua

--- file.js
+++ file.js
@@ -1,3 +1,9 @@
 function showMessage(){
     alert('Xin chào!');
 }
+function showMessage1(){
+    document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+}
+function showMessage3(){
+    document.write('Xin chào!');
+}
