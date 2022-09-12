c3dd3bec78c963c88004587f2429a7ab581ff29a
Nhathoang <hi1258820@gmail.com>
2022-09-12 11:38:48 +0700

    nopbai

---
+++ vidu.js
@@ -0,0 +1,11 @@
+function vidu(){
+    alert('Xin chào!');
+}
+
+function showMessage(){
+    document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+}
+
+function showMessage1(){
+    document.write('Xin chào!');
+}
