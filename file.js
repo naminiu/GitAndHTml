dd4ed7570c1baa9483eb18335beed4db30e587e5
luulo123123 <luulo3527@gmail.com>
2022-09-12 21:27:20 +0700

    update

--- file.js
+++
@@ -1,9 +0,0 @@
-function showMessage(){
-    alert('Xin chào!');
-}
-function showMessage1(){
-    document.getElementById('messageDiv').innerHTML = 'Xin chào!';
-}
-function showMessage3(){
-    document.write('Xin chào!');
-}
