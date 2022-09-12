e4402af87d767a06c00ff9cd9b77e38525b05ed6
Nhathoang <hi1258820@gmail.com>
2022-09-12 16:14:24 +0700

    THSudungtoantu

---
+++ math.js
@@ -0,0 +1,6 @@
+inputWidth = prompt("Nhập chiều rộng: ");
+inputHeight = prompt("Nhập chiều dài: ");
+var width = parseInt(inputWidth);
+var height = parseInt(inputHeight);
+var area = width * height;
+document.write("The area is: "+ area);
