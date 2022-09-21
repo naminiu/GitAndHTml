66bc91b44cb79cbc18bfe6c6d5de1d11dd339d69
Nhathoang <hi1258820@gmail.com>
2022-09-21 15:26:37 +0700

    nopbai

--- CauTrucLap2/BTHienThiCacSoNguyenDauTien.js
+++ CauTrucLap2/BTHienThiCacSoNguyenDauTien.js
@@ -1,11 +1,14 @@
-let number = 20;
+let number = 10
 let countOfDivisor = 0;
     for (let divisor = 2; divisor < number; divisor++) {
         if (number % divisor == 0) {
-            console.log(number + " k phai la so ngto");
+            console.log(number + " khong phai la so ngto");
+            countOfDivisor++;
             break;
         }
     }
     if (countOfDivisor == 0) {
-        console.log(number + " la so ngto");
+    //    document.getElementById("result1").innerHTML = number;
+    // document.write(number + " ");
+   console.log(number + " la so ngto");
     }
